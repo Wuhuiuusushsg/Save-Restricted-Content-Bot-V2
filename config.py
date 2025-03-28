0f0fc7b733f5e1830ab1e1a24b82b9772649478d
Arjun <99074495+Wuhuiuusushsg@users.noreply.github.com>
2025-03-28 14:59:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "20054245"))
 API_HASH = getenv("API_HASH", "431f22f320ed5d69225d3b3fc253fc0d")
-BOT_TOKEN = getenv("BOT_TOKEN", "8024462099:AAFzUcopYQfcN3U0EDp5vEin_PdxlnXmyiA")
+BOT_TOKEN = getenv("BOT_TOKEN", "8117739787:AAEFB7RuDBny6sR7lCtRbZom3LbI2VVqtZQ")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5034929962").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://motivationhubiit2025:75RgOXQzxYICxc30@cluster0.l6wktoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "1000"))
